cef3b1031bdb3704ceac66db8417f03b00fa6cd6
Joel West <emailjoelwest@gmail.com>
2023-02-17 14:35:23 -0500

    Finished task 2

--- hw3/story/story.js
+++ hw3/story/story.js
@@ -5,43 +5,58 @@ const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
 function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
+  const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
 
 // 2. RAW TEXT STRINGS
 
-// It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+const rawStory = "It was :inserttemp: outside, so :insertvillain: went for a walk. When they got to :insertlocation:, they stared in horror for a few moments, then :insertoutcome:. :insertname: saw the whole thing, but was not surprised — :insertvillain: weighs :insertweight:, and it was a hot day.";
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
+const villains = [
+  "Willy the Goblin",
+  "Big Daddy",
+  "Father Christmas"
+]
 
-// the soup kitchen
-// Disneyland
-// the White House
+const locations = [
+  "the soup kitchen",
+  "Disneyland",
+  "the White House"
+]
 
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and crawled away
+const outcomes = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and crawled away"
+]
 
 // 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
 function result() {
+  const name = (customName.value !== '') ? customName.value : "Bob";
 
-  if(customName.value !== '') {
-    const name = customName.value;
-
+  var weight;
+  var temperature;
+  if (document.getElementById("uk").checked) {
+    weight = "136 kilograms";
+    temperature =  "34 degrees celsius";
+  } else {
+    weight = "300 pounds";
+    temperature = "94 degrees fahrenheit";
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+  const villain = randomValueFromArray(villains);
+  const location = randomValueFromArray(locations);
+  const outcome = randomValueFromArray(outcomes);
 
-  }
-
-  story.textContent = ;
+  story.textContent = rawStory.replaceAll(":insertvillain:", villain)
+                              .replaceAll(":insertlocation:", location)
+                              .replaceAll(":insertoutcome:", outcome)
+                              .replaceAll(":insertname:", name)
+                              .replaceAll(":inserttemp:", temperature)
+                              .replaceAll(":insertweight:", weight);
   story.style.visibility = 'visible';
 }
