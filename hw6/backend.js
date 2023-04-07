4b3a3b35049d9f30bc06ca6a311e34fbfe5f72dd
Joel West <emailjoelwest@gmail.com>
2023-04-07 12:16:04 -0400

    Server receiving requests

--- hw6/backend.js
+++ hw6/backend.js
@@ -2,30 +2,44 @@
 const fs = require("fs");
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
+const cors = require("cors");
 
-// Create variables
+// Set up app
 const app = express();
+app.use((request, result, next) => {
+    result.setHeader("Access-Control-Allow-Origin", "*");
+    result.setHeader("Access-Control-Allow-Methods","POST");
+    result.setHeader("Access-Control-Allow-Methods", "Content-Type", "Authorization");
+    next();
+});
+
+// Create openai tools
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const baseImgStream = fs.readFileSync("img/stylish-guy.png");
+
+// Get image streams
+const baseImgStream = fs.createReadStream("img/stylish-guy.png");
 const maskImgStream = fs.createReadStream("img/mask.png");
 
 // Define endpoint
-app.post("/", function (request, response) {
-    const input = request.params.input;
+app.post("/dalle2", function (request, response) {
+    const input = request.query;
 
-    const dalleResponse = openai.createImageEdit(
-        baseImgStream,
-        maskImgStream,
-        "Man standing on the street wearing a " + input,
-        1,
-        "1024x1024"
-    );
+    console.log(input);
+    // const dalleResponse = openai.createImageEdit(
+    //     baseImgStream,
+    //     maskImgStream,
+    //     "Man standing on the street wearing a " + input,
+    //     1,
+    //     "1024x1024"
+    // );
 
-    const imgUrl = dalleResponse.then((val) => val.data.data[0].url);
-    response.send(imgUrl);
+    // const imgUrl = dalleResponse.then((val) => val.data.data[0].url);
+    // response.send(imgUrl);
 });
 
+app.use(express.json());
+app.use(cors());
 app.listen("8000");
--- hw6/dalle.js
+++ hw6/dalle.js
@@ -7,15 +7,18 @@ const searchForm = document.querySelector("form");
 const section = document.querySelector("section");
 
 // Event listeners to control the functionality
-searchForm.addEventListener("submit", submitSearch);
+searchForm.addEventListener("click", submitSearch);
 
-function submitSearch(input) {
-    input.preventDefault();
+var recentInput = "";
+
+function submitSearch(_) {
+    const input = searchTerm.value;
 
     // Make query to backend and get the url of response
-    // const xhr = new XMLHttpRequest();
-    // xhr.open("POST", "http://127.0.1.1:8000/");
-    // xhr.send("input=" + input);
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8000/dalle2?input=" + input);
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.send();
 
     // var imgUrl;
     // xhr.onload = function() {
@@ -23,31 +26,18 @@ function submitSearch(input) {
     //     imgUrl = JSON.parse(xhr.responseText);
     // }
 
-    const response = fetch("https://api.openai.com/v1/images/variations", {
-        method: "POST",
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Authorization": "Bearer $OPENAI_KEY",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: "{image='img/stylish-guy.png',mask='img/mask.png',prompt='A french man standing in a street',n=1,size='1024x1024'}", // body data type must match "Content-Type" header
-    });
+    // console.log(imgUrl);
 
-    console.log(imgUrl);
+    // const article = document.createElement("article");
+    // const img = document.createElement("img");
+    // const paragraph = document.createElement("p");
 
-    const article = document.createElement("article");
-    const img = document.createElement("img");
-    const paragraph = document.createElement("p");
+    // paragraph.textContent = current.snippet;
 
-    paragraph.textContent = current.snippet;
+    // img.src = imgUrl;
+    // img.alt = current.headline.main;
 
-    img.src = imgUrl;
-    img.alt = current.headline.main;
-
-    article.appendChild(img);
-    article.appendChild(paragraph);
-    section.appendChild(article);
+    // article.appendChild(img);
+    // article.appendChild(paragraph);
+    // section.appendChild(article);
 }
